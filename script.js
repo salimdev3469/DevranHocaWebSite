72555d477787aaf3ccbacddddbd999aff3c8394f
salimdev3469 <157348323+salimdev3469@users.noreply.github.com>
2024-07-05 12:49:14 +0300

    update1

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+//Site müziği
 window.addEventListener('load', function () {
     let music = document.getElementById('background-music');
     music.play().catch(function(error) {
         console.log("Playback error:", error);
+    });
+});
+
+// Devamını oku butonları düzenlemesi, her butonun da çalışması için foreach kullandık
+const readMoreBtns = document.querySelectorAll(".read-more-btn");
+const texts = document.querySelectorAll(".contentparag");
+
+readMoreBtns.forEach((btn, index) => {
+    btn.addEventListener("click", (e) => {
+        texts[index].classList.toggle("show-more");
+        if (btn.innerText === "Devamını oku") {
+            btn.innerText = "Kapat";
+        } else {
+            btn.innerText = "Devamını oku";
+        }
     });
 });
 
